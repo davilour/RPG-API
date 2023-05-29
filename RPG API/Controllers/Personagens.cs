c3e4e6db61fc66a0f61e827d64e117856284bd4c
Aluno <Aluno@SNCTP07LABF13>
2023-05-29 17:20:33 -0300

    Adicionar arquivos de projeto.

---
+++ RPG API/Controllers/Personagens.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RPG_API.Models;
+
+namespace RPG_API.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class Personagens : ControllerBase
+    {
+        private static List<Personagem> listaP = new List<Personagem>
+        {
+            new Personagem
+            {
+                Id = 1,
+                Nome = "Linkin",
+                Sobrenome = "Park",
+                Fantasia = "Banda rock",
+                Local = "USA"
+
+            },
+
+
+            new Personagem
+            {
+                Id = 2,
+                Nome = "Zelda",
+                Sobrenome = "Park",
+                Fantasia = "Babymetal",
+                Local = "USA"
+
+            }
+
+
+
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Personagem>>> TodosPersonagens()
+        {
+            return Ok(listaP);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Personagem>>> UnicoPersonagem(int id) //Pesquisa
+        {
+            var unico = listaP.Find(x => x.Id == id);
+
+            if (unico is null)
+                return NotFound("Não foi encontrado");
+
+            return Ok(unico);
+            
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Personagem>>> AddPersonagem(Personagem novo) //AddPersonagem
+        {
+            listaP.Add(novo);
+            return Ok(listaP);
+        }
+
+
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Personagem>>> EditaPersonagem(int id,Personagem editado)
+        {
+            var pesquisa = listaP.Find(x => x.Id == id);
+
+            if(pesquisa is null)
+                return NotFound("Personagem nao existe");
+
+            pesquisa.Nome = editado.Nome != "" && editado.Nome != "string" ? editado.Nome : pesquisa.Nome; //Expressao Ternaria 
+            pesquisa.Sobrenome = editado.Sobrenome != "" && editado.Sobrenome != "string" ? editado.Sobrenome : pesquisa.Sobrenome; //Expressao Ternaria 
+            pesquisa.Fantasia = editado.Fantasia != "" && editado.Fantasia != "string" ? editado.Fantasia : pesquisa.Fantasia; //Expressao Ternaria 
+            pesquisa.Local = editado.Local != "" && editado.Local != "string" ? editado.Local : pesquisa.Local; //Expressao Ternaria 
+
+               return Ok(pesquisa);
+            
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Personagem>>> DeletaPersonagem(int id)
+        {
+            var pesquisa = listaP.Find(x => x.Id==id);
+
+            if (pesquisa is null)
+                return NotFound("Personagem nao existe");
+
+            listaP.Remove(pesquisa);
+            return Ok(pesquisa);   
+
+
+        }
+
+    }
+}
---
+++ RPG API/Models/Personagem.cs
@@ -0,0 +1,14 @@
+﻿namespace RPG_API.Models
+{
+    public class Personagem
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string? Sobrenome { get; set; }
+        public string? Fantasia { get; set; }
+        public string? Local { get;set; }
+
+
+    }
+}
